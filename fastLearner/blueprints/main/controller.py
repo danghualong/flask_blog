6cca570da987a79f18f02883d0412cf048c2fb53
danghualong <dhl_345@qq.com>
2019-08-08 22:48:44 +0800

    增加index.html

--- fastLearner/blueprints/main/controller.py
+++ fastLearner/blueprints/main/controller.py
@@ -1,10 +1,13 @@
-from flask import render_template,current_app
+from flask import render_template
 from .forms import RegisterForm
 from . import main_bp
 from ...extensions import cache
 
 @main_bp.route('/register')
 def register():
-    current_app.logger.warning('start register')
     form=RegisterForm()
     return render_template('main/register.html',form=form)
+
+@main_bp.route('/')
+def index():
+    return render_template('main/index.html')
